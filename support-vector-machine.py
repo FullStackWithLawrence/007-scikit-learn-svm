fd9d869e3f069ae472ae29e7c40b0d044bfd55f5
lpm0073 <lpm0073@gmail.com>
2023-06-28 18:22:13 -0600

    refactor SVM model

---
+++ support-vector-machine.py
@@ -0,0 +1,156 @@
+# Encoding: utf-8
+"""
+    written by:     Lawrence McDaniel
+                    https://lawrencemcdaniel.com
+
+    date:           jun-2023
+
+    usage:          minimalist implementation of Logistic Regression model.
+"""
+import os
+import warnings
+
+# ------------------------------------------------------------------------------
+# IMPORTANT: DON'T FORGET TO INSTALL THESE LIBRARIES WITH pip
+# ------------------------------------------------------------------------------
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.preprocessing import MinMaxScaler
+# module initializations
+sns.set()
+HERE = os.path.abspath(os.path.dirname(__file__))
+warnings.filterwarnings("ignore")
+
+
+def metrics_score(actual, predicted):
+    """
+    Create a common function for measuring the
+    accuracy of both the train as well as test data.
+    """
+    print("Metrics Score.")
+    print(classification_report(actual, predicted))
+
+    cm = confusion_matrix(actual, predicted)
+    plt.figure(figsize=(8, 5))
+
+    sns.heatmap(
+        cm,
+        annot=True,
+        fmt=".2f",
+        xticklabels=["Not Cancelled", "Cancelled"],
+        yticklabels=["Not Cancelled", "Cancelled"],
+    )
+    plt.ylabel("Actual")
+    plt.xlabel("Predicted")
+    plt.show()
+
+
+def prepare_data():
+    """
+    Raw database transformations:
+        - clean the data
+        - remove columns that don't contain any information
+        - recast data types as necessary
+        - convert categorical data into series of dummy columns
+        - split dependent / independent variables
+        - split training / test data sets
+    """
+    original_db = pd.read_csv(os.path.join(HERE, "data", "reservations-db.csv"))
+
+    # need to be careful to only work with a **COPY** of the original
+    # source data, lest we accidentally permanently modify any of this
+    # raw data.
+    data = original_db.copy()
+
+    # remove the ID column from the data set, since it contains
+    # no predictive information.
+    data = data.drop(["Booking_ID"], axis=1)
+
+    # recast dependent variable as boolean
+    data["booking_status"] = data["booking_status"].apply(
+        lambda x: 1 if x == "Canceled" else 0
+    )
+
+    # hive off the dependent variable, "booking_status"
+    x = data.drop(["booking_status"], axis=1)
+    y = data["booking_status"]
+
+    # encode all categorical features
+    x = pd.get_dummies(x, drop_first=True)
+
+    # Split data in train and test sets
+    return train_test_split(x, y, test_size=0.30, stratify=y, random_state=1)
+
+
+def linear_kernal():
+    """
+    - create training and test data sets
+    - create a Logistic Regression model
+    - train the model
+    - generate confusion matrix and f-score for the training set
+    - generate confusion matrix and f-score for the test set
+    """
+    x_train, x_test, y_train, y_test = prepare_data()
+
+    scaling = MinMaxScaler(feature_range=(-1,1)).fit(x_train)
+    x_train_scaled = scaling.transform(x_train)
+    x_test_scaled = scaling.transform(x_test)
+
+    # Linear kernal or linear decision boundary
+    svm = SVC(kernel='linear', probability=True) 
+    model = svm.fit(X= x_train_scaled, y = y_train)
+
+    y_pred_train_svm = model.predict(x_train_scaled)
+    metrics_score(y_train, y_pred_train_svm)
+
+    y_pred_test_svm = model.predict(x_test_scaled)
+    metrics_score(y_test, y_pred_test_svm)
+
+    # Set the optimal threshold (refer to the Jupyter Notebook to see how we arrived at 42)
+    optimal_threshold_svm=0.40
+
+    y_pred_train_svm = model.predict_proba(x_train_scaled)
+    metrics_score(y_train, y_pred_train_svm[:,1]>optimal_threshold_svm)
+
+    y_pred_test = model.predict_proba(x_test_scaled)
+    metrics_score(y_test, y_pred_test[:,1]>optimal_threshold_svm)
+
+def rbf_kernal():
+    """
+    - create training and test data sets
+    - create a Logistic Regression model
+    - train the model
+    - generate confusion matrix and f-score for the training set
+    - generate confusion matrix and f-score for the test set
+    """
+    x_train, x_test, y_train, y_test = prepare_data()
+
+    scaling = MinMaxScaler(feature_range=(-1,1)).fit(x_train)
+    x_train_scaled = scaling.transform(x_train)
+    x_test_scaled = scaling.transform(x_test)
+
+    # Linear kernal or linear decision boundary
+    svm_rbf=SVC(kernel='rbf',probability=True)
+    model = svm_rbf.fit(x_train_scaled,y_train)
+
+    y_pred_train_svm = model.predict(x_train_scaled)
+    metrics_score(y_train, y_pred_train_svm)
+
+    y_pred_test_svm = model.predict(x_test_scaled)
+    metrics_score(y_test, y_pred_test_svm)
+
+    # Set the optimal threshold (refer to the Jupyter Notebook to see how we arrived at 42)
+    optimal_threshold_svm=0.41
+
+    y_pred_train_svm = model.predict_proba(x_train_scaled)
+    metrics_score(y_train, y_pred_train_svm[:,1]>optimal_threshold_svm)
+
+    y_pred_test = model.predict_proba(x_test_scaled)
+    metrics_score(y_test, y_pred_test[:,1]>optimal_threshold_svm)
+
+if __name__ == "__main__":
+    linear_kernal()
